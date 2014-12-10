d0ff5d849669b018bb2723d361d4b2890fb1acd2
delpillar <knivex27@gmail.com>
2014-12-10 10:11:02 -0500

    fix format

--- Scripts/states/level2.js
+++ Scripts/states/level2.js
@@ -9,26 +9,27 @@
 /// <reference path="../managers/collision.js" />
 
 var states;
+var count, constants;
 (function (states) {
     function level2State() {
         space.update();
         plane.update();
         
-        for (var count = 0; count < plane.bullets.length; count++){
+        for (count = 0; count < plane.bullets.length; count++) {
             plane.bullets[count].update();
         }
         
-        var interval = window.setInterval(function(){
+        var interval = window.setInterval(function () {
             window.clearInterval(interval);
             coin.update();
         
-            for (var count = 0; count < constants.ENEMY_NUM; count++) {
+            for (count = 0; count < constants.ENEMY_NUM; count++) {
                     enemies[count].update();
             }
             
             collision.update();
             scoreboard.update();
-        },1000);
+        }, 1000);
         
         if (scoreboard.lives <= 0) {
             stage.removeChild(game);
@@ -52,7 +53,7 @@ var states;
         coin = new objects.Coin(stage, game);
         enemy = new objects.Enemy(stage, game);
         plane = new objects.Plane(stage, game);
-
+        
         // Show Cursor
         stage.cursor = "none";
 
@@ -65,7 +66,8 @@ var states;
 
         // Instantiate Collision Manager
         collision = new managers.Collision(plane, coin, lasers, scoreboard, enemies, plane.bullets);
-
+        window.addEventListener("keydown", plane.pressKey);
+        window.addEventListener("keyup", plane.releaseKey);
         stage.addChild(game);
     }
     states.level2 = level2;
